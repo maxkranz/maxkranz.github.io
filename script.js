ce5daf4afe4a27e8caa89c806c85b2668a3b8412
Max Cranz <151439927+maxcranz@users.noreply.github.com>
2024-03-15 15:51:13 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    let nam = prompt ('What is your name?');
+    if ("Max"==nam){
+    alert("We have the same name! Horray!")
+    }
+    
+    else {
+        alert("We haven't the same names")
+    }
