f26310c44796cd4372ae892c7a161f6091550fc1
Max Cranz <151439927+maxkranz@users.noreply.github.com>
2024-06-02 09:27:16 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('static-v1').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/app.js',
+                '/icon.png',
+                '/icon-512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
