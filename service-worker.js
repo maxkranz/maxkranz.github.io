1261a2c60f73b2110b3531d70cc535b0626e9702
Max Cranz <151439927+maxkranz@users.noreply.github.com>
2024-06-02 09:29:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,11 +3,8 @@ self.addEventListener('install', function(event) {
         caches.open('static-v1').then(function(cache) {
             return cache.addAll([
                 '/',
-                '/index.html',
-                '/styles.css',
-                '/app.js',
-                '/icon.png',
-                '/icon-512.png'
+                '/amandagl.html',
+                '/Снимок экрана от 2024-05-24 09-46-24.png'
             ]);
         })
     );
