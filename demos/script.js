353130308b90448fb658f1beef8e41b22d56f76e
max <gmax.nikolaev@gmail.com>
2024-12-18 08:13:15 +0100

    cleaning!

---
+++ demos/script.js
@@ -0,0 +1,9 @@
+let nam = prompt("What is your name?");
+const names = ["Maxim", "Max"];
+
+if (names.includes(nam)){
+    alert("we have the same names!")
+}
+else{
+    alert("we havent the same names!")
+}
--- script.js
+++
@@ -1,9 +0,0 @@
-let nam = prompt("What is your name?");
-const names = ["Maxim", "Max"];
-
-if (names.includes(nam)){
-    alert("we have the same names!")
-}
-else{
-    alert("we havent the same names!")
-}
